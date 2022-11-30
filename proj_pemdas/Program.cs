0345f69e2147d95db2952d31692c68a8ebc52e16
Ezarsatria12 <Ezar3004>
2022-11-30 19:52:06 +0700

    <1>

--- proj_pemdas/Program.cs
+++ proj_pemdas/Program.cs
@@ -6,6 +6,9 @@ namespace proj_pemdas
     {
         static void Main(string[] args)
         {
+            //setup
+            Console.SetWindowSize(120, 40);
+            Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
         }
     }
