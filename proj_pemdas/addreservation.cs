92ea51586f3cc5cfb97736070a879e9a8ea8f879
Ezarsatria12 <Ezar3004>
2022-12-01 23:39:51 +0700

    <read data>

--- proj_pemdas/addreservation.cs
+++ proj_pemdas/addreservation.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
 
 namespace proj_pemdas
 {
@@ -13,6 +16,47 @@ namespace proj_pemdas
             Console.WriteLine("####################");
             Console.WriteLine("# ini reservasi #");
             Console.WriteLine("#####################");
+            addreservation data = new addreservation();
+            data.GetUserDetails();
         }
-    }
+        private void GetUserDetails()
+        {
+            var json = File.ReadAllText("E:\\MMS\\kuliah\\proj_pemdas\\proj_pemdas\\data.json");
+            try
+            {
+                var jObject = JObject.Parse(json);
+
+                if (jObject != null)
+                {
+                    Console.WriteLine("ID :" + jObject["id"].ToString());
+                    Console.WriteLine("Name :" + jObject["name"].ToString());
+
+                    var address = jObject["address"];
+                    Console.WriteLine("Street :" + address["street"].ToString());
+                    Console.WriteLine("City :" + address["city"].ToString());
+                    Console.WriteLine("Zipcode :" + address["zipcode"]);
+                    JArray experiencesArrary = (JArray)jObject["experiences"];
+                    if (experiencesArrary != null)
+                    {
+                        foreach (var item in experiencesArrary)
+                        {
+                            Console.WriteLine("company Id :" + item["companyid"]);
+                            Console.WriteLine("company Name :" + item["companyname"].ToString());
+                        }
+
+                    }
+                    Console.WriteLine("Phone Number :" + jObject["phoneNumber"].ToString());
+                    Console.WriteLine("Role :" + jObject["role"].ToString());
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }   
+    
 }
